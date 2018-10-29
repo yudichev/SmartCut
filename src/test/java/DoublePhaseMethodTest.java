4c31d4f18b0ef3835f279d08428d18d64f7848ce
Valery Yudichev <valery.yudichev@yandex.ru>
2018-10-30 01:36:27 +0300

    Develop doublePhase method

--- src/test/java/DoublePhaseMethodTest.java
+++ src/test/java/DoublePhaseMethodTest.java
@@ -1,33 +1,37 @@
 import math.linear.basic.*;
 import math.linear.simplex.canonical.CanonicalProblem;
 import math.linear.simplex.canonical.DoublePhaseMethod;
-import math.linear.simplex.canonical.SinglePhaseMethod;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 public class DoublePhaseMethodTest {
 
     @Test
     public void testDoublePhase() {
-        Equation eq1 = Equation.of(new double[]{3., 2.}, Relation.GREATER_OR_EQUAL, 3.);
-        Equation eq2 = Equation.of(new double[]{5., 4.}, Relation.GREATER_OR_EQUAL, 6.);
+        Equation eq1 = Equation.of(new double[]{5., 1., -2., 3.}, Relation.GREATER_OR_EQUAL, 12.);
+        Equation eq2 = Equation.of(new double[]{1., 3., 0., 2.}, Relation.GREATER_OR_EQUAL, 23.);
+        Equation eq3 = Equation.of(new double[]{3., 2., 1., 7.}, Relation.GREATER_OR_EQUAL, 45.);
+        Equation eq4 = Equation.of(new double[]{3., -7., 1., 1.}, Relation.GREATER_OR_EQUAL, 64.);
         EquationSet eqset = EquationSet.create();
         eqset.addEquation(eq1);
         eqset.addEquation(eq2);
+        eqset.addEquation(eq3);
+        eqset.addEquation(eq4);
 
-        ObjectiveFunction objfunc = ObjectiveFunction.create(new double[]{1., 2.}, ObjectiveFunctionType.MINIMUM);
+        ObjectiveFunction objfunc = ObjectiveFunction.create(new double[]{7., 3., 1., 2.}, ObjectiveFunctionType.MINIMUM);
 
         CanonicalProblem problem = CanonicalProblem.create(eqset, objfunc);
 
-        CanonicalProblem problem2 = DoublePhaseMethod.processFirstPhase(problem);
+        CanonicalProblem problem2 = DoublePhaseMethod.prepareAuxilieryFunction(problem);
 
-        CanonicalProblem problem3 = CanonicalProblem.create(problem2.getEquationSet(), problem2.getAuxFunction().getCanonical());
+        CanonicalProblem problem3 = DoublePhaseMethod.solveAuxiliery(problem2);
 
-        double[] solution = SinglePhaseMethod.solve(problem3);
+        CanonicalProblem problem4 = DoublePhaseMethod.solve(problem3);
 
-        Assert.assertArrayEquals(new double[]{6./5., 0.}, solution, 0.0d);
+        double[] solution = DoublePhaseMethod.extractSolution(problem4);
+
+
+        Assert.assertArrayEquals(new double[]{0., 0.,36.,28.}, solution, 0.0d);
 
     }
 }
