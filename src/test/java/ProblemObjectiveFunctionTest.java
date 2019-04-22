51f2f4494cbd9dd6aea05dce1bd056da4872e0de
CORP-HR\Valery.Yudichev <valery.yudichev@hireright.com>
2019-04-22 18:17:03 +0300

    Fix

--- src/test/java/ProblemObjectiveFunctionTest.java
+++ src/test/java/ProblemObjectiveFunctionTest.java
@@ -13,12 +13,11 @@ public class ProblemObjectiveFunctionTest
 {
     @Test
     public void testCreate(){
-        double[] val1 = new double[] {1., 2., 3.};
+        double[] val1 = new double[] {1.d, 2.d, 3.d};
         ProblemObjectiveFunction fun1 = ProblemObjectiveFunction.make(val1, ObjectiveFunctionType.MAXIMUM);
-        Assert.assertEquals(1., fun1.getCoefficientAt(1), 0.0d);
-        Assert.assertEquals(2., fun1.getCoefficientAt(2), 0.0d);
-        Assert.assertEquals(3., fun1.getCoefficientAt(3), 0.0d);
-        Assert.assertArrayEquals(val1,fun1.getCoefficients(),0.0d);
+        Assert.assertEquals(1.d, fun1.getCoefficientAt(1), 0.0d);
+        Assert.assertEquals(2.d, fun1.getCoefficientAt(2), 0.0d);
+        Assert.assertEquals(3.d, fun1.getCoefficientAt(3), 0.0d);
         Assert.assertEquals(ObjectiveFunctionType.MAXIMUM, fun1.getType());
         Assert.assertEquals(fun1.getType(),ObjectiveFunctionType.MAXIMUM);
 
